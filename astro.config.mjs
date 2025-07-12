50cc3b691b4a99b9af86e861f8a82bfc6bde61a7
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2025-07-13 00:01:43 +0100

    add pragmaticity post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.jeseromero.com',
-  base: 'linkaynn.github.io'
+  base: process.env.NODE_ENV === 'production' ? 'linkaynn.github.io' : '/'
 });
